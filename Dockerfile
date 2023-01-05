0078e3922827fcd665d4f66d478a1b6df2e459fc
Ivan Reshetnikov <resivalex@gmail.com>
2023-01-05 18:53:03 +0600

    Run in docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:14-alpine3.12
+
+WORKDIR /app
+
+ADD ["package.json", "package-lock.json", "./"]
+
+RUN yarn install
+
+ADD public public
+ADD src src
+ADD [".env", "tsconfig.json", "./"]
+
+RUN yarn build
+
+CMD ["yarn", "run", "serve", "--single", "build", "--listen", "tcp://0.0.0.0:3000"]
--- src/Contact.tsx
+++ src/Contact.tsx
@@ -5,7 +5,6 @@ export default function Contact() {
     first: 'Your',
     last: 'Name',
     avatar: 'https://placekitten.com/g/200/200',
-    twitter: 'your_handle',
     notes: 'Some notes',
     favorite: true,
   }
@@ -13,7 +12,7 @@ export default function Contact() {
   return (
     <div id="contact">
       <div>
-        <img key={contact.avatar} src={contact.avatar || ''} />
+        <img alt="" key={contact.avatar} src={contact.avatar || ''} />
       </div>
 
       <div>
@@ -28,32 +27,7 @@ export default function Contact() {
           <Favorite contact={contact} />
         </h1>
 
-        {contact.twitter && (
-          <p>
-            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
-              {contact.twitter}
-            </a>
-          </p>
-        )}
-
         {contact.notes && <p>{contact.notes}</p>}
-
-        <div>
-          <Form action="edit">
-            <button type="submit">Edit</button>
-          </Form>
-          <Form
-            method="post"
-            action="destroy"
-            onSubmit={(event) => {
-              if (!window.confirm('Please confirm you want to delete this record.')) {
-                event.preventDefault()
-              }
-            }}
-          >
-            <button type="submit">Delete</button>
-          </Form>
-        </div>
       </div>
     </div>
   )
