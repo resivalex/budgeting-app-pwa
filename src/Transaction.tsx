38967e3549860cd9707c966368c758fd75605805
Ivan Reshetnikov <resivalex@gmail.com>
2023-01-08 21:08:43 +0600

    Fix transaction layout

--- src/Transaction.tsx
+++ src/Transaction.tsx
@@ -14,14 +14,14 @@ class TransactionDTO {
 export default function Transaction({ t }: { t: TransactionDTO }) {
   return (
     <div className="box m-1">
-      <div className="columns">
-        <div className="column">
+      <div className="is-flex is-justify-content-space-between">
+        <div>
           <div className="has-text-weight-semibold">{t.category}</div>
           <div>{t.account}</div>
           <div className="is-size-7 has-text-weight-semibold">{t.payee}</div>
           <div className="is-size-7">{t.comment}</div>
         </div>
-        <div className="column">
+        <div className="has-text-right">
           <div className="is-size-5">
             {t.amount} {t.currency}
           </div>
