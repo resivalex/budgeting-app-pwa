e50ac764ec55f38c47ec4e4d587d0a6f42fc1298
Ivan Reshetnikov <resivalex@gmail.com>
2023-01-15 00:23:51 +0300

    Sync local db with remote

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { Routes, Route, Link, useLocation } from 'react-router-dom'
-import axios from 'axios'
 import Home from './Home'
 import Transactions from './Transactions'
 import NotFound from './NotFound'
 import classNames from 'classnames'
 import Config from './Config'
+import PouchDB from 'pouchdb'
+
+type ConfigType = {
+  backendUrl: string
+  backendToken: string
+  dbUrl: string
+}
 
 export default function App() {
   const [transactions, setTransactions] = useState([])
@@ -18,14 +24,46 @@ export default function App() {
       if (!window.localStorage.config) {
         return
       }
-      const config = JSON.parse(window.localStorage.config)
+      const config: ConfigType = JSON.parse(window.localStorage.config)
       try {
-        const response = await axios.get(config.backendUrl + '/transactions', {
-          params: {
-            token: config.backendToken,
-          },
-        })
-        setTransactions(response.data)
+        const localDB = new PouchDB('budgeting')
+        const remoteDB = new PouchDB(config.dbUrl + '/budgeting')
+        localDB
+          .sync(remoteDB, {
+            live: true,
+            retry: true,
+          })
+          .on('change', (info: any) => {
+            console.log('DB change', info)
+          })
+          .on('paused', (err: any) => {
+            console.log('DB paused', err)
+          })
+          .on('active', () => {
+            console.log('DB active')
+          })
+          .on('denied', (err: any) => {
+            console.log('DB denied', err)
+          })
+          .on('complete', (info: any) => {
+            console.log('DB complete', info)
+          })
+          .on('error', (err: any) => {
+            console.log('DB error', err)
+          })
+
+        localDB
+          .allDocs({
+            include_docs: true,
+          })
+          .then(function (result: any) {
+            // Extract the documents from the result
+            const docs = result.rows.map((row: any) => row.doc)
+            setTransactions(docs)
+          })
+          .catch(function (err: any) {
+            setError(err)
+          })
       } catch (err: any) {
         setError(err.toString())
       } finally {
