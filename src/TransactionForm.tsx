4e7c50ccd7d03bd72880d2f6d8667d7418a337bf
Ivan Reshetnikov <resivalex@gmail.com>
2023-03-26 18:30:59 +0600

    Add other transaction inputs

--- src/TransactionForm.tsx
+++ src/TransactionForm.tsx
@@ -7,6 +7,12 @@ type Props = {
 
 export default function Transaction({ onAdd }: Props) {
   const [amount, setAmount] = useState('')
+  const [account, setAccount] = useState('')
+  const [category, setCategory] = useState('')
+  const [type, setType] = useState<'income' | 'expense' | 'transfer'>('expense')
+  const [currency, setCurrency] = useState('')
+  const [payee, setPayee] = useState('')
+  const [comment, setComment] = useState('')
 
   return (
     <div className="field has-addons p-2">
@@ -19,18 +25,71 @@ export default function Transaction({ onAdd }: Props) {
         />
       </div>
       <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Account"
+          onChange={(e) => setAccount(e.target.value)}
+        />
+      </div>
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Category"
+          onChange={(e) => setCategory(e.target.value)}
+        />
+      </div>
+      <div className="control">
+        <select
+          className="input"
+          onChange={(e) => {
+            const type = e.target.value as 'income' | 'expense' | 'transfer'
+            setType(type)
+          }}
+        >
+          <option value="expense">Expense</option>
+          <option value="income">Income</option>
+          <option value="transfer">Transfer</option>
+        </select>
+      </div>
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Currency"
+          onChange={(e) => setCurrency(e.target.value)}
+        />
+      </div>
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Payee"
+          onChange={(e) => setPayee(e.target.value)}
+        />
+      </div>
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Comment"
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </div>
+      <div className="control">
         <button
           className="button is-info"
           onClick={() =>
             onAdd({
               datetime: new Date().toISOString().substring(0, 19).replace('T', ' '),
-              account: 'Ваня Карта',
-              category: 'Кафе',
-              type: 'expense',
+              account: account,
+              category: category,
+              type: type,
               amount: `${amount}.00`,
-              currency: 'RUB',
-              payee: 'Тестовое',
-              comment: 'На что-то',
+              currency: currency,
+              payee: payee,
+              comment: comment,
             })
           }
         >
