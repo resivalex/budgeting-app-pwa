d3f3a79465fd2aeff46cb434d9ebbd2d73077d44
Ivan Reshetnikov <resivalex@gmail.com>
2023-04-15 17:18:18 +0600

    Move the rest logics to AppContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useRef } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Status from './Status'
 import Transactions from './Transactions'
 import NotFound from './NotFound'
 import TransactionForm from './TransactionForm'
 import Login from './Login'
-import DbService from './DbService'
-import TransactionAggregator from './TransactionAggregator'
 import Menu from './Menu'
-import BackendService from './BackendService'
 import Notification from './Notification'
 import { TransactionDTO } from './Transaction'
 
@@ -20,18 +17,14 @@ type AppProps = {
   isLoading: boolean
   offlineMode: boolean
   lastNotificationText: string
-  onSetIsAuthenticated: (value: boolean) => void
-  onSetTransactions: (transactions: any) => void
-  onSetError: (error: string) => void
-  onSetIsLoading: (value: boolean) => void
-  onSetOfflineMode: (value: boolean) => void
-  onSetLastNotificationText: (text: string) => void
-}
-
-type ConfigType = {
-  backendUrl: string
-  backendToken: string
-  dbUrl: string
+  onLogout: () => void
+  onAddTransaction: (transaction: TransactionDTO) => void
+  onRemoveTransaction: (id: string) => void
+  onSuccessfulLogin: () => void
+  onCloseError: () => void
+  onDismissNotification: () => void,
+  accountDetails: any,
+  sortedCategories: any,
 }
 
 const appVersion = '20230414-1700'
@@ -44,107 +37,15 @@ export default function App(props: AppProps) {
     isLoading,
     offlineMode,
     lastNotificationText,
-    onSetIsAuthenticated,
-    onSetTransactions,
-    onSetError,
-    onSetIsLoading,
-    onSetOfflineMode,
-    onSetLastNotificationText,
+    onLogout,
+    onAddTransaction,
+    onRemoveTransaction,
+    onSuccessfulLogin,
+    onCloseError,
+    onDismissNotification,
+    accountDetails,
+    sortedCategories,
   } = props
-
-  const dbServiceRef = useRef<DbService | null>(null)
-
-  useEffect(() => {
-    if (window.localStorage.config) {
-      onSetIsAuthenticated(true)
-    }
-  }, [])
-
-  const navigate = useNavigate()
-
-  const transactionAggregator = new TransactionAggregator(transactions)
-  const accountDetails = transactionAggregator.getAccountDetails()
-  const sortedCategories = transactionAggregator.getSortedCategories()
-
-  const handleLogout = () => {
-    localStorage.removeItem('config')
-    window.location.reload()
-  }
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      return
-    }
-    async function loadTransactions() {
-      if (!window.localStorage.config) {
-        return
-      }
-      const config: ConfigType = JSON.parse(window.localStorage.config)
-
-      const backendService = new BackendService(config.backendUrl, config.backendToken)
-      let settings = null
-      try {
-        settings = await backendService.getSettings()
-      } catch (err) {
-        onSetOfflineMode(true)
-      }
-
-      const shouldReset = settings
-        ? async () => {
-            const checkSettings = await backendService.getSettings()
-            const changed =
-              window.localStorage.transactionsUploadedAt !== checkSettings.transactionsUploadedAt
-            if (changed) {
-              window.localStorage.transactionsUploadedAt = checkSettings.transactionsUploadedAt
-            }
-            return changed
-          }
-        : async () => false
-
-      if (dbServiceRef.current) {
-        return
-      }
-      const dbService = new DbService({
-        dbUrl: config.dbUrl,
-        onLoading: onSetIsLoading,
-        onDocsRead: onSetTransactions,
-        onError: onSetError,
-        shouldReset: shouldReset,
-      })
-      dbServiceRef.current = dbService
-      if (settings) {
-        const resetDb =
-          window.localStorage.transactionsUploadedAt !== settings.transactionsUploadedAt
-        if (resetDb) {
-          window.localStorage.transactionsUploadedAt = settings.transactionsUploadedAt
-          await dbService.reset()
-        }
-      }
-      await dbService.synchronize()
-    }
-    void loadTransactions()
-  }, [isAuthenticated])
-
-  async function addTransaction(t: TransactionDTO) {
-    const dbService: DbService | null = dbServiceRef.current
-    if (!dbService) {
-      return
-    }
-
-    await dbService.addTransaction(t)
-    onSetLastNotificationText('Transaction added')
-    navigate('/transactions')
-  }
-
-  async function removeTransaction(id: string) {
-    const dbService: DbService | null = dbServiceRef.current
-    if (!dbService) {
-      return
-    }
-
-    await dbService.removeTransaction(id)
-    onSetLastNotificationText('Transaction removed')
-  }
 
   return (
     <div>
@@ -153,9 +54,7 @@ export default function App(props: AppProps) {
           message={lastNotificationText}
           type="is-success"
           duration={1500}
-          onDismiss={() => {
-            onSetLastNotificationText('')
-          }}
+          onDismiss={onDismissNotification}
         />
       )}
       {isAuthenticated ? (
@@ -167,7 +66,7 @@ export default function App(props: AppProps) {
             backgroundColor: offlineMode ? 'gray' : 'white',
           }}
         >
-          <Menu handleLogout={handleLogout} appVersion={appVersion} offlineMode={offlineMode} />
+          <Menu handleLogout={onLogout} appVersion={appVersion} offlineMode={offlineMode} />
           <div
             style={{
               width: '100%',
@@ -184,13 +83,15 @@ export default function App(props: AppProps) {
               />
               <Route
                 path="/transactions"
-                element={<Transactions transactions={transactions} onRemove={removeTransaction} />}
+                element={
+                  <Transactions transactions={transactions} onRemove={onRemoveTransaction} />
+                }
               />
               <Route
                 path="/add"
                 element={
                   <TransactionForm
-                    onAdd={addTransaction}
+                    onAdd={onAddTransaction}
                     accounts={accountDetails}
                     categories={sortedCategories}
                   />
@@ -201,9 +102,9 @@ export default function App(props: AppProps) {
           </div>
         </div>
       ) : (
-        <Login onSuccessfulLogin={() => onSetIsAuthenticated(true)} />
+        <Login onSuccessfulLogin={onSuccessfulLogin} />
       )}
-      <Status isLoading={isLoading} error={error} onClose={() => onSetError('')} />
+      <Status isLoading={isLoading} error={error} onClose={onCloseError} />
     </div>
   )
 }
--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { AppState } from './redux/types'
 import {
   setIsAuthenticated,
@@ -10,39 +11,141 @@ import {
   setLastNotificationText,
 } from './redux/appSlice'
 import App from './App'
+import DbService from './DbService'
+import BackendService from './BackendService'
+import { TransactionDTO } from './Transaction'
+import TransactionAggregator from './TransactionAggregator'
+
+type ConfigType = {
+  backendUrl: string
+  backendToken: string
+  dbUrl: string
+}
 
 export default function AppContainer() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const isAuthenticated = useSelector((state: AppState) => state.isAuthenticated)
   const transactions = useSelector((state: AppState) => state.transactions)
   const error = useSelector((state: AppState) => state.error)
   const isLoading = useSelector((state: AppState) => state.isLoading)
   const offlineMode = useSelector((state: AppState) => state.offlineMode)
   const lastNotificationText = useSelector((state: AppState) => state.lastNotificationText)
+  const dbServiceRef = useRef<DbService | null>(null)
+
+  useEffect(() => {
+    if (window.localStorage.config) {
+      dispatch(setIsAuthenticated(true))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return
+    }
+    async function loadTransactions() {
+      if (!window.localStorage.config) {
+        return
+      }
+      const config: ConfigType = JSON.parse(window.localStorage.config)
+
+      const backendService = new BackendService(config.backendUrl, config.backendToken)
+      let settings = null
+      try {
+        settings = await backendService.getSettings()
+      } catch (err) {
+        dispatch(setOfflineMode(true))
+      }
+
+      const shouldReset = settings
+        ? async () => {
+            const checkSettings = await backendService.getSettings()
+            const changed =
+              window.localStorage.transactionsUploadedAt !== checkSettings.transactionsUploadedAt
+            if (changed) {
+              window.localStorage.transactionsUploadedAt = checkSettings.transactionsUploadedAt
+            }
+            return changed
+          }
+        : async () => false
+
+      if (dbServiceRef.current) {
+        return
+      }
+      const dbService = new DbService({
+        dbUrl: config.dbUrl,
+        onLoading: (value) => dispatch(setIsLoading(value)),
+        onDocsRead: (docs) => dispatch(setTransactions(docs)),
+        onError: (err) => dispatch(setError(err)),
+        shouldReset: shouldReset,
+      })
+      dbServiceRef.current = dbService
+      if (settings) {
+        const resetDb =
+          window.localStorage.transactionsUploadedAt !== settings.transactionsUploadedAt
+        if (resetDb) {
+          window.localStorage.transactionsUploadedAt = settings.transactionsUploadedAt
+          await dbService.reset()
+        }
+      }
+      await dbService.synchronize()
+    }
+    void loadTransactions()
+  }, [isAuthenticated])
+
+  async function addTransaction(t: TransactionDTO) {
+    const dbService: DbService | null = dbServiceRef.current
+    if (!dbService) {
+      return
+    }
+
+    await dbService.addTransaction(t)
+    dispatch(setLastNotificationText('Transaction added'))
+    navigate('/transactions')
+  }
+
+  async function removeTransaction(id: string) {
+    const dbService: DbService | null = dbServiceRef.current
+    if (!dbService) {
+      return
+    }
+
+    await dbService.removeTransaction(id)
+    dispatch(setLastNotificationText('Transaction removed'))
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('config')
+    window.location.reload()
+  }
 
   const handleSetIsAuthenticated = (value: boolean) => {
     dispatch(setIsAuthenticated(value))
-  }
-
-  const handleSetTransactions = (transactions: any) => {
-    dispatch(setTransactions(transactions))
   }
 
   const handleSetError = (error: string) => {
     dispatch(setError(error))
   }
 
-  const handleSetIsLoading = (value: boolean) => {
-    dispatch(setIsLoading(value))
-  }
-
-  const handleSetOfflineMode = (value: boolean) => {
-    dispatch(setOfflineMode(value))
-  }
-
   const handleSetLastNotificationText = (text: string) => {
     dispatch(setLastNotificationText(text))
   }
+
+  const handleSuccessfulLogin = () => {
+    handleSetIsAuthenticated(true)
+  }
+
+  const handleCloseError = () => {
+    handleSetError('')
+  }
+
+  const handleDismissNotification = () => {
+    handleSetLastNotificationText('')
+  }
+
+  const transactionAggregator = new TransactionAggregator(transactions)
+  const accountDetails = transactionAggregator.getAccountDetails()
+  const sortedCategories = transactionAggregator.getSortedCategories()
 
   return (
     <App
@@ -52,12 +155,14 @@ export default function AppContainer() {
       isLoading={isLoading}
       offlineMode={offlineMode}
       lastNotificationText={lastNotificationText}
-      onSetIsAuthenticated={handleSetIsAuthenticated}
-      onSetTransactions={handleSetTransactions}
-      onSetError={handleSetError}
-      onSetIsLoading={handleSetIsLoading}
-      onSetOfflineMode={handleSetOfflineMode}
-      onSetLastNotificationText={handleSetLastNotificationText}
+      onLogout={handleLogout}
+      onAddTransaction={addTransaction}
+      onRemoveTransaction={removeTransaction}
+      onSuccessfulLogin={handleSuccessfulLogin}
+      onCloseError={handleCloseError}
+      onDismissNotification={handleDismissNotification}
+      accountDetails={accountDetails}
+      sortedCategories={sortedCategories}
     />
   )
 }
