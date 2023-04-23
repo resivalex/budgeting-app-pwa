71b69bae39d2a477462f0d3589a20c6cd4ebf10a
Ivan Reshetnikov <resivalex@gmail.com>
2023-04-24 00:08:41 +0600

    Fix transfer filtering

--- src/TransactionPageContainer.tsx
+++ src/TransactionPageContainer.tsx
@@ -12,6 +12,9 @@ export default function TransactionsPageContainer({ transactions, onRemove }: Pr
 
   const filteredTransactions = transactions.filter((transaction) => {
     if (filterAccountName === '') return true
+    if (transaction.type === 'transfer' && transaction.payee === filterAccountName) {
+      return true
+    }
     return transaction.account === filterAccountName
   })
 
