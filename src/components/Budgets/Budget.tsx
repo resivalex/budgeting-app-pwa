e8663f4e670539426d4427d15f8ed33924512998
Ivan Reshetnikov <resivalex@gmail.com>
2023-05-13 21:17:54 +0600

    Output spent amount

--- src/components/Budgets/Budget.tsx
+++ src/components/Budgets/Budget.tsx
@@ -11,54 +11,39 @@ interface Props {
   onLongPress: () => void
 }
 
-function outputAmountDifference(totalAmount: number, spentAmount: number, currency: string) {
-  const currencySymbol = convertCurrencyCodeToSymbol(currency)
-
-  function outputBeforeTotalAmount(
-    totalAmount: number,
-    spentAmount: number,
-    currencySymbol: string
-  ) {
-    if (spentAmount > totalAmount) {
-      return (
-        <>
-          Перерасход{' '}
-          <span className="has-text-weight-bold">
-            {formatFinancialAmountRounded(spentAmount - totalAmount)} {currencySymbol}
-          </span>
-        </>
-      )
-    }
-    if (spentAmount < 0) {
-      return (
-        <>
-          Доход{' '}
-          <span className="has-text-weight-bold">
-            {formatFinancialAmountRounded(-spentAmount)} {currencySymbol}
-          </span>
-        </>
-      )
-    }
-    if (spentAmount <= totalAmount) {
-      return (
-        <>
-          Осталось{' '}
-          <span className="has-text-weight-bold">
-            {formatFinancialAmountRounded(totalAmount - spentAmount)} {currencySymbol}
-          </span>
-        </>
-      )
-    }
+function outputAmountDifference(
+  totalAmount: number,
+  spentAmount: number,
+  currency: string,
+  currencySymbol: string
+) {
+  if (spentAmount > totalAmount) {
+    return (
+      <>
+        <span className="has-text-weight-bold" style={{ color: '#8f2626' }}>
+          -{formatFinancialAmountRounded(spentAmount - totalAmount)} {currencySymbol}
+        </span>
+      </>
+    )
   }
-
-  return (
-    <>
-      {outputBeforeTotalAmount(totalAmount, spentAmount, currencySymbol)} /{' '}
-      <span className="has-text-weight-bold">
-        {formatFinancialAmountRounded(totalAmount)} {currencySymbol}
-      </span>
-    </>
-  )
+  if (spentAmount < 0) {
+    return (
+      <>
+        <span className="has-text-weight-bold" style={{ color: '#23672e' }}>
+          +{formatFinancialAmountRounded(-spentAmount)} {currencySymbol}
+        </span>
+      </>
+    )
+  }
+  if (spentAmount <= totalAmount) {
+    return (
+      <>
+        <span className="has-text-weight-bold">
+          {formatFinancialAmountRounded(totalAmount - spentAmount)} {currencySymbol}
+        </span>
+      </>
+    )
+  }
 }
 
 export default function Budget({ totalAmount, spentAmount, currency, name, onLongPress }: Props) {
@@ -69,15 +54,24 @@ export default function Budget({ totalAmount, spentAmount, currency, name, onLon
     cancelOnMovement: true,
     detect: LongPressDetectEvents.BOTH,
   })
+  const currencySymbol = convertCurrencyCodeToSymbol(currency)
 
   return (
     <div {...longPressBind()} className="box my-2 py-2">
       <div className="is-flex is-justify-content-space-between pb-1">
         <div className="has-text-weight-bold is-flex pr-1">{name}</div>
+        <div className="has-text-weight-bold is-flex pl-1" style={{ color: '#c7c7c7' }}>
+          {formatFinancialAmountRounded(totalAmount)} {currencySymbol}
+        </div>
       </div>
       <BudgetProgressBar totalAmount={totalAmount} spentAmount={spentAmount} />
-      <div className="is-flex pt-1 is-justify-content-right">
-        <div>{outputAmountDifference(totalAmount, spentAmount, currency)}</div>
+      <div className="is-flex is-justify-content-space-between pt-1">
+        <div className="has-text-weight-bold is-flex pr-1">
+          {formatFinancialAmountRounded(spentAmount)} {currencySymbol}
+        </div>
+        <div className="is-flex pl-1">
+          {outputAmountDifference(totalAmount, spentAmount, currency, currencySymbol)}
+        </div>
       </div>
     </div>
   )
