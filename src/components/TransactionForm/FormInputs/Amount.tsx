3d45b97f012391dba3e3d232fe9823353d09a1d9
Ivan Reshetnikov <resivalex@gmail.com>
2023-06-17 17:14:36 +0600

    Focus amount input

--- src/components/TransactionForm/FormInputs/Amount.tsx
+++ src/components/TransactionForm/FormInputs/Amount.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useRef } from 'react'
+
 interface Props {
   amount: string
   onAmountChange: (amount: string) => void
 }
 
 export default function Amount({ amount, onAmountChange }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    // focus on mount
+    inputRef.current?.focus()
+  }, [])
   return (
     <div className="field">
       <div className="is-size-7">Сумма</div>
       <div className="control">
         <input
+          ref={inputRef}
           className="input is-small"
           type="number"
           placeholder="Сумма"
