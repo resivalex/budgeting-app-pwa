71ff90d38da35b84d1f7737c26e6069151988812
Ivan Reshetnikov <resivalex@gmail.com>
2023-06-25 22:30:46 +0600

    `AccountSelect` wrappter in `FormInputs/Account`

--- src/components/TransactionForm/FormInputs/Account.tsx
+++ src/components/TransactionForm/FormInputs/Account.tsx
@@ -1,30 +1,44 @@
 import { reactSelectColorStyles } from '@/utils'
 import Select from 'react-select'
 
-interface Props {
+export default function Account({
+  account,
+  onAccountChange,
+  accountOptions,
+}: {
   account: string
   onAccountChange: (account: string) => void
   accountOptions: { value: string; label: string; color: string }[]
-}
+}) {
+  function AccountSelect({
+    value,
+    onChange,
+  }: {
+    value: string
+    onChange: (value: string) => void
+  }) {
+    return (
+      <Select
+        className="basic-single"
+        classNamePrefix="select"
+        value={accountOptions.find((option) => option.value === value) || null}
+        onChange={(selectedOption) => {
+          if (!selectedOption) return
+          onChange(selectedOption.value)
+        }}
+        options={accountOptions}
+        isSearchable={false}
+        placeholder="Выберите из списка..."
+        styles={reactSelectColorStyles}
+      />
+    )
+  }
 
-export default function Account({ account, onAccountChange, accountOptions }: Props) {
   return (
     <div className="field">
       <div className="is-size-7">Счёт</div>
       <div className="control">
-        <Select
-          className="basic-single"
-          classNamePrefix="select"
-          value={accountOptions.find((option) => option.value === account) || null}
-          onChange={(selectedOption) => {
-            if (!selectedOption) return
-            onAccountChange(selectedOption.value)
-          }}
-          options={accountOptions}
-          isSearchable={false}
-          placeholder="Выберите из списка..."
-          styles={reactSelectColorStyles}
-        />
+        <AccountSelect value={account} onChange={onAccountChange} />
       </div>
     </div>
   )
