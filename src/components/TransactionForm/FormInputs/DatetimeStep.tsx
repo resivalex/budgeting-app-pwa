c737e8e850bed347fc9cd7f31c690b6051e5517b
Ivan Reshetnikov <resivalex@gmail.com>
2024-03-24 13:33:23 +0500

    Debounce datetime change

--- src/components/TransactionForm/FormInputs/DatetimeStep.tsx
+++ src/components/TransactionForm/FormInputs/DatetimeStep.tsx
@@ -1,12 +1,14 @@
+import { useCallback, useState } from 'react'
 import styled from 'styled-components'
 import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { convertToLocaleTime } from '@/utils'
-import ru from 'date-fns/locale/ru';
+import ru from 'date-fns/locale/ru'
+import { debounce } from 'lodash'
 
 interface Props {
-  datetime: Date
+  initialDatetime: Date
   isExpanded: boolean
   onDatetimeChange: (datetime: Date | null) => void
   onExpand: () => void
@@ -21,7 +23,26 @@ const SelectedDateTime = styled.div`
   font-size: 0.8rem;
 `
 
-export default function DatetimeStep({ datetime, isExpanded, onDatetimeChange, onExpand }: Props) {
+export default function DatetimeStep({
+  initialDatetime,
+  isExpanded,
+  onDatetimeChange,
+  onExpand,
+}: Props) {
+  const [datetime, setDatetime] = useState(initialDatetime)
+
+  const debouncedOnDatetimeChange = useCallback(
+    debounce((newDatetime: Date | null) => {
+      onDatetimeChange(newDatetime)
+    }, 1000),
+    []
+  )
+
+  const handleDatetimeChange = (newDatetime: Date | null) => {
+    setDatetime(newDatetime || new Date())
+    debouncedOnDatetimeChange(newDatetime)
+  }
+
   if (!isExpanded) {
     return (
       <div className="field" onClick={onExpand}>
@@ -42,7 +63,7 @@ export default function DatetimeStep({ datetime, isExpanded, onDatetimeChange, o
         <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ru}>
           <DesktopDateTimePicker
             value={datetime}
-            onChange={onDatetimeChange as any}
+            onChange={handleDatetimeChange}
             views={['year', 'month', 'day', 'hours', 'minutes', 'seconds']}
             viewRenderers={{
               hours: null,
