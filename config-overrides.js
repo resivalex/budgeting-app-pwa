be4369d9b450ed0c6b8ad2233e037849a6d529ad
Ivan Reshetnikov <resivalex@gmail.com>
2025-04-24 20:32:58 +0500

    Enable React Compiler

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const { override, addWebpackAlias } = require('customize-cra')
+const { addBabelPlugin } = require('customize-cra')
 
 module.exports = override(
   addWebpackAlias({
     '@': path.resolve(__dirname, 'src'),
-  })
+  }),
+  addBabelPlugin(['babel-plugin-react-compiler', {}])
 )
